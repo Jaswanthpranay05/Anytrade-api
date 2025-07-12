a13170f6f3f2ee0f1170cb0dba1eec842c5aae63
PALURU JASWANTH <155042203+Jaswanthpranay05@users.noreply.github.com>
2025-07-12 13:49:20 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const express = require('express');
+const admin = require('firebase-admin');
+const mongoose = require('./config/db'); // Mongo connection
+const registerRoute = require('./routes/register');
+const serviceAccount = require('./firebase/serviceAccountKey.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const app = express();
+app.use(express.json());
+
+app.use('/api', registerRoute);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
---
+++ models/User.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  uid: { type: String, required: true, unique: true },
+  name: String,
+  email: String,
+  phone: String,
+  domain: String
+});
+
+module.exports = mongoose.model('User', userSchema);
