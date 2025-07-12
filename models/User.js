70b183630463204e77b5fe9c5d90b1e9196d1232
PALURU JASWANTH <155042203+Jaswanthpranay05@users.noreply.github.com>
2025-07-12 16:37:03 +0530

    User.js

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   uid: { type: String, required: true, unique: true },
@@ -15,7 +16,30 @@ const userSchema = new mongoose.Schema({
     required: true,
     match: [/^\d{10}$/, 'Phone number must be 10 digits']
   },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6
+  },
   domain: { type: String, required: true }
 });
+
+// Pre-save hook to hash password
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next(); // only hash if password is new or modified
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Method to verify password
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
 
 module.exports = mongoose.model('User', userSchema);
